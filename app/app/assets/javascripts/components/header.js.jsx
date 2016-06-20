e438df474aeb46f5627a46baa29ede46ca2c31c2
Karl Jensen <karljensen@gmail.com>
2016-06-20 14:03:50 -0700

    Added Google Font for header title.

--- app/app/assets/javascripts/components/header.js.jsx
+++ app/app/assets/javascripts/components/header.js.jsx
@@ -1,5 +1,5 @@
 var Header = React.createClass({
     render: function() {
-        return ( <div className="title-panel"></div> );
+        return ( <div className="title-panel"><h1>Roll For It</h1></div> );
     }
 });
