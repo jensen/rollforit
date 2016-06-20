5397733bca499b718ae941e6a31437b93c669c2c
Karl Jensen <karljensen@gmail.com>
2016-06-20 16:02:56 -0700

    Simple formatting updates for consistency.

--- app/app/controllers/cards_controller.rb
+++ app/app/controllers/cards_controller.rb
@@ -1,5 +1,5 @@
 class CardsController < ApplicationController
-  def index
-      render json: "[ [1, 3], [1, 2, 5, 6], [3, 3, 3], [1, 1, 3, 3, 6, 6] ]" 
-  end
+    def index
+        render json: "[ [1, 3], [1, 2, 5, 6], [3, 3, 3], [1, 1, 3, 3, 6, 6] ]"
+    end
 end
