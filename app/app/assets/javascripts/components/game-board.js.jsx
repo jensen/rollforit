e35a49e0923c810ef7aad1022bda0e592894bd3d
Karl Jensen <karljensen@gmail.com>
2016-06-26 02:05:50 -0700

    Clean up ReactJS stuff.

--- app/app/assets/javascripts/components/game-board.js.jsx
+++
@@ -1,9 +0,0 @@
-var GameBoard = React.createClass({
-    render: function() {
-        return (
-            <div className="game-board">
-                <CardTray url='/cards' />
-            </div>
-        );
-    }
-});
--- app/app/assets/javascripts/components/header.js.jsx
+++
@@ -1,5 +0,0 @@
-var Header = React.createClass({
-    render: function() {
-        return ( <div className="title-panel"><h1>Roll For It</h1></div> );
-    }
-});
