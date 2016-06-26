7db04ea7de2518381d1800e5a0093a3fd339fee8
Karl Jensen <karljensen@gmail.com>
2016-06-26 02:11:31 -0700

    Controller clean up.
    - Controllers for cards and dice were only sending stub data.

--- app/app/controllers/cards_controller.rb
+++
@@ -1,5 +0,0 @@
-class CardsController < ApplicationController
-    def index
-        render json: "[ [1, 3], [1, 2, 5, 6], [3, 3, 3], [1, 1, 3, 3, 6, 6] ]"
-    end
-end
--- app/app/controllers/dice_controller.rb
+++
@@ -1,5 +0,0 @@
-class DiceController < ApplicationController
-    def index
-        render json: "[ 1, 2, 0, 4, 5, 6 ]"
-    end
-end
