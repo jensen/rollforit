2aa9a350009194f260cb076e3c116831f710f0a6
Karl Jensen <karljensen@gmail.com>
2016-07-04 12:07:25 -0700

    Remove old templates and test cases for old login flow.

--- app/test/controllers/games_controller_test.rb
+++ app/test/controllers/games_controller_test.rb
@@ -1,8 +1,4 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
-    test "should get create game form" do
-        get :new
-        assert_response :success
-    end
 end
--- app/test/controllers/players_controller_test.rb
+++ app/test/controllers/players_controller_test.rb
@@ -1,32 +1,4 @@
 require 'test_helper'
 
 class PlayersControllerTest < ActionController::TestCase
-    test "should get create player form" do
-        get :new, params: { game_id: games(:game).id }
-
-        assert_response :success
-    end
-
-    test "should fail to create player with invalid data" do
-        post :create, params: {
-            game_id: games(:game).id,
-            player: {
-                :name => "",
-                :slot => nil,
-                :game_id => nil } }
-
-        assert_response :success
-        assert_select 'h2', "Error"
-    end
-
-    test "should create new player via html request" do
-        post :create, params: {
-            game_id: games(:game).id,
-            player: {
-                :name => players(:malloy).name,
-                :slot => players(:malloy).slot,
-                :game_id => games(:game).id } }
-
-        assert_redirected_to game_path(assigns(:game))
-    end
 end
